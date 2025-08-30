bcce5d00f47887492f7ce21d9b6611cb2b2b325b
Kushagra4647verma <kushagra4647@gmail.com>
2025-08-30 20:14:16 +0530

    Correct cards

--- frontend/src/pages/UserDashboard.jsx
+++ frontend/src/pages/UserDashboard.jsx
@@ -9,7 +9,7 @@ import {
 import API from "@/utils/api";
 import { Card, CardContent } from "@/components/ui/card";
 import { useState, useEffect } from "react";
-
+import { Button } from "@/components/ui/button";
 export function Userdashboard() {
   const [courses, setCourses] = useState([]);
   useEffect(() => {
@@ -30,21 +30,42 @@ export function Userdashboard() {
     </div>
   );
 }
-
 function Courses({ courses }) {
+  const [message, setmessage] = useState(null);
+  async function purchaseCourse(id) {
+    try {
+      const res = await API.post(`/users/${id}/buy`);
+      setmessage(res.data.message);
+      alert(res.data.message);
+    } catch (err) {
+      setmessage("Error in purchasing course!");
+      alert("Error in purchasing course!");
+    }
+  }
   return (
     <div className="w-full max-w-4xl mx-auto flex justify-center items-center min-h-screen">
       <Carousel>
         <CarouselContent>
-          {courses.map((course, index) => (
-            <CarouselItem key={index} className="basis-full">
+          {courses.map((course) => (
+            <CarouselItem key={course._id} className="basis-full">
               <Card className="p-8 rounded-2xl shadow-md w-full">
                 <CardContent>
-                  <h2 className="text-4xl font-bold">{course.title}</h2>
-                  <br />
-                  <p className="text-l text-gray-500 text-justify">
+                  <h2 className="text-4xl font-bold mb-4">{course.title}</h2>
+
+                  <p className="text-lg text-gray-500 text-justify mb-6">
                     {course.description}
                   </p>
+
+                  <div className="flex items-center justify-between gap-4">
+                    <h3 className="text-2xl font-semibold">₹{course.price}</h3>
+
+                    <Button
+                      onClick={() => purchaseCourse(course._id)}
+                      className="ml-auto"
+                    >
+                      Purchase
+                    </Button>
+                  </div>
                 </CardContent>
               </Card>
             </CarouselItem>
